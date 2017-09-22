1cc9cfbcda4247f8d390a46c88031e93a2606be1
buckethead1986 <rspeckpfs@gmail.com>
2017-09-21 23:15:02 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
+    # self.slice(-1) == "." #other method
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
-  def count_sentences
-
+  def count_sentences #how do I split along '.' but not '...' in one line?
+    self.split(/[.!?]/).reject {|element| element.empty? }.count #removes blank '""' elements.
   end
 end
